8fb5991d17ccd80dad30a5238bd1826e79b9e364
MrGoodtrips <serhatozturk.mr.goodtrips@gmail.com>
2022-02-17 00:16:01 +0300

    Add project files.

---
+++ MVCPartialDevamTekrar_1/ViewModels/HomeVM.cs
@@ -0,0 +1,15 @@
+﻿using MVCPartialDevamTekrar_1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCPartialDevamTekrar_1.ViewModels
+{
+    public class HomeVM
+    {
+        public List<Category> Categories { get; set; }
+        public List<Product> Products { get; set; }
+        public List<Employee> Employees { get; set; }
+    }
+}
